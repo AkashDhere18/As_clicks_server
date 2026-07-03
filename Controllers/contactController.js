b58fe42896e0645c53bfc35b51e59d448cb9212c
AkashDhere18 <dhereakash18@gmail.com>
2026-07-04 01:06:16 +0530

    Used Gmail SMTP

--- Controllers/contactController.js
+++ Controllers/contactController.js
@@ -7,13 +7,17 @@ const sendContactMail = async (req, res) => {
         const { name, email, contact, event, message } = req.body;
 
         const transporter = nodemailer.createTransport({
-            service: "gmail",
-
+            host: "smtp.gmail.com",
+            port: 587,
+            secure: false,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS,
             },
         })
+
+        await transporter.verify();
+        console.log("SMTP Connected Successfully");
 
         await transporter.sendMail({
             from: process.env.EMAIL_USER,
